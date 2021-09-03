77f8a192dfb52f8c13a7258c97354845fc95b015
Nastya <leloneyagmail.com>
2021-09-03 19:45:09 +0300

    Created models Item, User, WatchList and Price, using django-money lib. Added django-money to pipfile and settings. Added dotenv lib to work with .env file

--- trade_platform/trade/models.py
+++ trade_platform/trade/models.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from djmoney.models.fields import MoneyField
 
-# Create your models here.
+
+class User(AbstractUser):
+    balance = MoneyField(max_digits=14, decimal_places=2, default=0, default_currency='USD', blank=True, null=True)
+
+
+class Item(models.Model):
+    code = models.CharField(max_length=8, unique=True)
+    name = models.CharField(max_length=125, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Price(models.Model):
+    """An actual price on item due to date"""
+    actual_price = MoneyField(max_digits=14, decimal_places=2, default_currency='USD', blank=True, null=True)
+    actual_date = models.DateTimeField(unique=True, blank=True, null=True)
+    item = models.ForeignKey(Item, blank=True, null=True, on_delete=models.SET_NULL,
+                             related_name='prices', related_query_name='prices')
+
+
+class WatchList(models.Model):
+    user = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
+    item = models.ForeignKey(Item, blank=True, null=True, on_delete=models.SET_NULL)
+
+
+
--- trade_platform/trade_platform/settings.py
+++ trade_platform/trade_platform/settings.py
@@ -10,7 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+from dotenv import load_dotenv
 from pathlib import Path
+
+
+load_dotenv()
+
+AUTO_CONVERT_MONEY = True
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -33,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'trade.apps.TradeConfig',
+    'djmoney',
+    'djmoney.contrib.exchange',
 ]
 
 MIDDLEWARE = [
