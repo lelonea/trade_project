ab26f39b6058ca418967444cedcc4844e7c695d0
Nastya <leloneyagmail.com>
2021-09-06 16:49:07 +0300

    Edited blanks. Edited path in admin.py
    Edited blanks. Edited path in admin.py

--- trade_platform/trade/models.py
+++ trade_platform/trade/models.py
@@ -21,17 +21,15 @@ class Item(models.Model):
 class Price(models.Model):
     """An actual price on item due to date"""
     actual_price = MoneyField(max_digits=14, decimal_places=2, default_currency='USD', blank=True, null=True)
-    actual_date = models.DateTimeField(auto_now=True)
-    item = models.ForeignKey(Item, blank=True, null=True, on_delete=models.SET_NULL,
+    actual_date = models.DateTimeField(auto_now=True, null=True)
+    item = models.ForeignKey(Item, null=True, on_delete=models.SET_NULL,
                              related_name='prices', related_query_name='prices')
 
 
 class WatchList(models.Model):
     """Favorite list of stocks belongs to user"""
-    user = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL,
-                             related_name='user_watchlist', related_query_name='user_watchlist')
-    item = models.ForeignKey(Item, blank=True, null=True, on_delete=models.SET_NULL,
-                             related_name='item_watchlist', related_query_name='item_watchlist')
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name='user_watchlist')
+    item = models.ForeignKey(Item, blank=True, null=True, on_delete=models.SET_NULL, related_name='item_watchlist')
 
 
 class Offer(models.Model):
@@ -41,30 +39,28 @@ class Offer(models.Model):
         (2, 'Buy'),
     )
 
-    user = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL,
-                             related_name='user_offer', related_query_name='user_offer')
-    item = models.ForeignKey(Item, blank=True, null=True, on_delete=models.SET_NULL,
-                             related_name='item_offer', related_query_name='item_offer')
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name='user_offer')
+    item = models.ForeignKey(Item, null=True, on_delete=models.SET_NULL, related_name='item_offer')
     order_type = models.PositiveSmallIntegerField(choices=ORDER_TYPE)
     entry_quantity = models.IntegerField('Requested quantity')
     quantity = models.IntegerField('Current quantity')
-    price = MoneyField(max_digits=14, decimal_places=2, default_currency='USD')
+    price = MoneyField(max_digits=14, decimal_places=2, default_currency='USD', null=True)
     is_active = models.BooleanField(default=True)
     date_and_time = models.DateTimeField(auto_now_add=True)
 
 
 class Trade(models.Model):
     """Information about a certain transaction"""
-    item = models.ForeignKey(Item, blank=True, null=True, on_delete=models.SET_NULL)
+    item = models.ForeignKey(Item, null=True, on_delete=models.SET_NULL)
     quantity = models.IntegerField(default=0)
     unit_price = MoneyField(max_digits=14, decimal_places=2, default_currency='USD', blank=True, null=True)
-    seller = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL,
+    seller = models.ForeignKey(User, null=True, on_delete=models.SET_NULL,
                                related_name='seller_trade', related_query_name='seller_trade')
-    buyer = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL,
+    buyer = models.ForeignKey(User, null=True, on_delete=models.SET_NULL,
                               related_name='buyer_trade', related_query_name='buyer_trade')
-    seller_offer = models.ForeignKey(Offer, blank=True, null=True, on_delete=models.SET_NULL,
+    seller_offer = models.ForeignKey(Offer, null=True, on_delete=models.SET_NULL,
                                      related_name='seller_trade', related_query_name='seller_trade')
-    buyer_offer = models.ForeignKey(Offer, blank=True, null=True, on_delete=models.SET_NULL,
+    buyer_offer = models.ForeignKey(Offer, null=True, on_delete=models.SET_NULL,
                                     related_name='buyer_trade', related_query_name='buyer_trade')
     description = models.TextField(blank=True, null=True)
     date_and_time = models.DateTimeField(auto_now_add=True)
